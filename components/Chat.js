80fe8e5b7919cbb6d62e2973ecc62314131af2e9
Alin Georgian, Antoci <alin.georgian.antoci@gmail.com>
2021-04-05 17:48:30 +0300

    Building 1-1 chat page SSR

--- components/Chat.js
+++ components/Chat.js
@@ -4,8 +4,10 @@ import getRecipientEmail from '../utils/getRecipientEmail';
 import { auth, db } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollection } from 'react-firebase-hooks/firestore';
+import { useRouter } from 'next/router';
 
 function Chat({ id, users }) {
+  const router = useRouter();
   const [user] = useAuthState(auth);
   const [recipientSnapshot] = useCollection(
     db.collection('users').where('email', '==', getRecipientEmail(users, user))
@@ -14,8 +16,12 @@ function Chat({ id, users }) {
   const recipientEmail = getRecipientEmail(users, user);
   const recipient = recipientSnapshot?.docs?.[0]?.data();
 
+  const enterChat = () => {
+    router.push(`/chat/${id}`);
+  };
+
   return (
-    <Container>
+    <Container onClick={enterChat}>
       {recipient ? (
         <UserAvatar src={recipient?.photoURL} />
       ) : (
---
+++ components/ChatScreen.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+function ChatScreen() {
+  return <Container></Container>;
+}
+
+export default ChatScreen;
+
+const Container = styled.div``;
